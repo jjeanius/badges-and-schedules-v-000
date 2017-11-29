894e3f5d6dabebe388549b21a3c5478a66e0e4b0
Judy McGoldrick <jjeanius@verizon.net>
2017-11-29 19:24:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,8 +19,8 @@ def assign_rooms(attendees)
   end
 end
 
-  def printer(attendee)
-    puts badge_maker(attendee)
+  def printer(attendees)
+    puts batch_badge_creator(attendees)
     puts "#{assign_rooms}"
     binding.pry
   end
