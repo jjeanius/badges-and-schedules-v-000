5aecc1f5d7da77ab879951b92cbc04f9249a290b
Judy McGoldrick <jjeanius@verizon.net>
2017-11-29 18:50:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require "pry"
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  # attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.map do |attendee|
+    badge_maker(attendee)
+  end
+end
+
+def assign_rooms(attendees)
+  count = 0
+    attendees.map do |attendee|
+    count += 1
+    "Hello, #{attendee}! You'll be assigned to room #{count}!"
+  end
+end
+
+  def printer(attendees)
+    puts batch_badge_creator(attendees)
+    puts "#{assign_rooms}"
+    binding.pry
+  end
